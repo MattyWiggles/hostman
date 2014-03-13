ec052f0d8284d41cd56b429d70a506eb2b410c70
GLanata <gavinlanata@hotmail.com>
2014-03-13 16:36:18 +0000

    Stying on the editing window, delete button added. Changed main window from combo box to listbox for design reasons.

---
+++ HostmanUI/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HostmanUI.Converters
+{
+    class BooleanToVisibilityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null || value.GetType() != typeof(bool))
+                return Visibility.Collapsed;
+
+            if (parameter != null)
+                value = !(bool)value;
+
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
